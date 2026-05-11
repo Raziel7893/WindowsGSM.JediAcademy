0a270fbb4dd0b7ef683c8b7dfac3c5536161ca1e
raziel7893 <raziel7893@gmail.com>
2026-05-11 23:14:49 +0200

    initial

---
+++ JediAcademy.cs/JediAcademy.cs
@@ -0,0 +1,225 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Net;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+using WindowsGSM.Functions;
+using WindowsGSM.GameServer.Engine;
+using WindowsGSM.GameServer.Query;
+
+namespace WindowsGSM.Plugins
+{
+    public class JediAcademy : SteamCMDAgent
+    {
+        // - Plugin Details
+        public Plugin Plugin = new Plugin
+        {
+            name = "WindowsGSM.JediAcademy", // WindowsGSM.XXXX
+            author = "MuchLive - Twitch.Tv/Much_Live",
+            description = "WindowsGSM plugin for supporting JediAcademy Dedicated Server",
+            version = "1.0.1",
+            url = "", // Github repository link (Best practice) TODO
+            color = "#34FFeb" // Color Hex
+        };
+
+        // - Settings properties for SteamCMD installer
+        public override bool loginAnonymous => false;
+        public override string AppId => "6020"; // Game server appId Steam
+
+        // - Standard Constructor and properties
+        public JediAcademy(ServerConfig serverData) : base(serverData) => base.serverData = serverData;
+
+        // - Game server Fixed variables
+        //public override string StartPath => "JediAcademyServer.exe"; // Game server start path
+        public override string StartPath => "jampDed.exe";
+        public string GamePath => "JediAcademy.exe";
+        public string FullName = "JediAcademy Dedicated Server"; // Game server FullName
+
+        public bool AllowsEmbedConsole = false;  // Does this server support output redirect?
+        public int PortIncrements = 11; // This tells WindowsGSM how many ports should skip after installation
+
+        // - Game server default values
+        public string Port = "29060"; // Default port
+        public string Additional = "+set dedicated 1 +exec server.cfg"; // Additional server start parameter
+
+        // TODO: Following options are not supported yet, as ther is no documentation of available options
+        public string Maxplayers = "10"; // Default maxplayers        
+        public string QueryPort = "29070"; // Default query port. This is the port specified in the Server Manager in the client UI to establish a server connection.
+        // TODO: Unsupported option
+        public string Defaultmap = "Dedicated"; // Default map name
+        // TODO: Undisclosed method
+        public object QueryMethod = new A2S(); // Query method should be use on current server type. Accepted value: null or new A2S() or new FIVEM() or new UT3()
+
+
+
+        // - Create a default cfg for the game server after installation
+        public async void CreateServerCFG()
+        {
+
+        }
+
+        public async Task<bool> InstallServerFile()
+        {
+            const string ServerUrl = "https://files.jkhub.org/jka/official/jawinded_1.011.zip";
+            string exePath = Functions.ServerPath.GetServersServerFiles(serverData.ServerID, StartPath);
+            string configPath = Functions.ServerPath.GetServersServerFiles(serverData.ServerID, "server.cfg");
+            string zipPath = Functions.ServerPath.GetServersServerFiles(serverData.ServerID, "jawinded.zip");
+
+            if (File.Exists(exePath)) { File.Delete(exePath); }
+            if (File.Exists(zipPath)) { File.Delete(zipPath); }
+
+            try
+            {
+                using (WebClient webClient = new WebClient())
+                {
+                    //Run jre-8u231-windows-i586-iftw.exe to install Java
+                    await webClient.DownloadFileTaskAsync(new Uri(ServerUrl), zipPath);
+                    await FileManagement.ExtractZip(zipPath, Functions.ServerPath.GetServersServerFiles(serverData.ServerID));
+                    if (!File.Exists(configPath))
+                    {
+                        File.Copy(Functions.ServerPath.GetServersServerFiles(serverData.ServerID, "base", "server.cfg"), configPath);
+                    }
+
+                    if (File.Exists(exePath))
+                    {
+                        File.Delete(zipPath);
+                        return true;
+                    }
+                    else
+                    {
+                        Error = $"File {exePath} does not seem to exist after extracting {zipPath}";
+                        return false;
+                    }
+                }
+            }
+            catch
+            {
+                Error = $"Could not download or extract jawinded.zip from {ServerUrl}";
+            }
+            return false;
+        }
+
+        // - Start server function, return its Process to WindowsGSM
+        public async Task<Process> Start()
+        {
+            string shipExePath = Functions.ServerPath.GetServersServerFiles(serverData.ServerID, StartPath);
+            if (!File.Exists(shipExePath))
+            {
+                if (!(await InstallServerFile()))
+                {
+                    Error = $"{Path.GetFileName(shipExePath)} not found ({shipExePath})";
+                    return null;
+                }
+            }
+
+            // Prepare Process
+            var p = new Process
+            {
+                StartInfo =
+                {
+                    CreateNoWindow = false,
+                    WorkingDirectory = ServerPath.GetServersServerFiles(serverData.ServerID),
+                    FileName = shipExePath,
+                    Arguments = serverData.ServerParam,
+                    WindowStyle = ProcessWindowStyle.Minimized,
+                    UseShellExecute = false
+                },
+                EnableRaisingEvents = true
+            };
+
+            // Set up Redirect Input and Output to WindowsGSM Console if EmbedConsole is on
+            if (serverData.EmbedConsole)
+            {
+                p.StartInfo.RedirectStandardInput = true;
+                p.StartInfo.RedirectStandardOutput = true;
+                p.StartInfo.RedirectStandardError = true;
+                p.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+                p.StartInfo.CreateNoWindow = true;
+                var serverConsole = new ServerConsole(serverData.ServerID);
+                p.OutputDataReceived += serverConsole.AddOutput;
+                p.ErrorDataReceived += serverConsole.AddOutput;
+            }
+
+            // Start Process
+            try
+            {
+                p.Start();
+                if (serverData.EmbedConsole)
+                {
+                    p.BeginOutputReadLine();
+                    p.BeginErrorReadLine();
+                }
+                return p;
+            }
+            catch (Exception e)
+            {
+                Error = e.Message;
+                return null; // return null if fail to start
+            }
+        }
+
+        public new bool IsInstallValid()
+        {
+            string installPath = Functions.ServerPath.GetServersServerFiles(serverData.ServerID, GamePath);
+            Error = $"Fail to find {installPath}";
+            return File.Exists(installPath);
+        }
+
+        public new bool IsImportValid(string path)
+        {
+            string importPath = Path.Combine(path, GamePath);
+            Error = $"Invalid Path! Fail to find {Path.GetFileName(GamePath)}";
+            return File.Exists(importPath);
+        }
+
+        // - Stop server function
+        public async Task Stop(Process p)
+        {
+            await Task.Run(() =>
+            {
+                if (!SendStopSignal(p))
+                {
+                    p.Kill();
+                }
+            });
+        }
+
+        #region preparation of the WindowsAPI to send process shutdown signals
+        internal const int CTRL_C_EVENT = 0;
+        [DllImport("kernel32.dll")]
+        internal static extern bool GenerateConsoleCtrlEvent(uint dwCtrlEvent, uint dwProcessGroupId);
+        [DllImport("kernel32.dll", SetLastError = true)]
+        internal static extern bool AttachConsole(uint dwProcessId);
+        [DllImport("kernel32.dll", SetLastError = true, ExactSpelling = true)]
+        internal static extern bool FreeConsole();
+        [DllImport("kernel32.dll")]
+        static extern bool SetConsoleCtrlHandler(ConsoleCtrlDelegate HandlerRoutine, bool Add);
+        delegate Boolean ConsoleCtrlDelegate(uint CtrlType);
+        #endregion
+
+        //sends the stop signal to the process
+        public static bool SendStopSignal(Process p)
+        {
+            if (AttachConsole((uint)p.Id))
+            {
+                SetConsoleCtrlHandler(null, true);
+                try
+                {
+                    if (!GenerateConsoleCtrlEvent(CTRL_C_EVENT, 0))
+                    {
+                        return false;
+                    }
+                    p.WaitForExit(10000);
+                }
+                finally
+                {
+                    SetConsoleCtrlHandler(null, false);
+                    FreeConsole();
+                }
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,90 @@
+# WindowsGSM.JediAcademy
+🧩WindowsGSM plugin that provides JediAcademy Dedicated server
+
+## PLEASE ⭐STAR⭐ THE REPO IF YOU LIKE IT! THANKS!
+
+### Notes
+- Use Import to use another install method as Steam. 
+- The ports (or rather any option besides Server Start Param) are not used for the game, only to reserve against other servers, no idea how to set those, seem fixed
+- To Configure
+* Click Edit Config => and check Server Start Param. Add +set fs_game modName to add mods. Dont forget a space between options
+* Click Browse => Server Files and edit server.cfg to your liking
+
+### WindowsGSM Installation: 
+1. Download  WindowsGSM https://windowsgsm.com/ 
+2. Create a Folder at a Location you wan't all Server to be Installed and Run.
+3. Drag WindowsGSM.Exe into previously created folder and execute it.
+
+### Plugin Installation:
+1. Download [latest](https://https://github.com/Raziel7893/WindowsGSM.JediAcademy/releases/latest) release
+   Or click Code => Download .zip
+3. Either Extract then Move the folder **JediAcademy.cs** to **WindowsGSM/plugins** 
+    1. Press on the Puzzle Icon in the left bottom side and press **[RELOAD PLUGINS]** or restart WindowsGSM
+4. Or Press on the Puzzle Icon in the left bottom side and press **[IMPORT PLUGIN]** and choose the downloaded .zip
+
+### Official Documentation
+🗃️ https://jkhub.org/tutorials/server-management/creating-a-jedi-academy-or-outcast-dedicated-or-private-server-home-run-for-windows-r208/
+
+### The Game
+🕹️ https://store.steampowered.com/app/6020/STAR_WARS_Jedi_Knight__Jedi_Academy
+
+### Dedicated server info
+🖥️ https://steamdb.info/app/6020/infos/
+
+### Port Forwarding (YOU NEED THIS, TO BE ABLE TO CONNECT FROM THE INTERNET(only for servers/pcs at home):
+- If You don't know How: Google: YourRouterBrand + Portforwarding
+- No Idead what ports the game use tbh. 
+
+### Files To Backup
+- Save Game (You could only save serverfiles/JediAcademy/Saved , but that includes many big logs)
+  - WindowsGSM\servers\%ID%\serverfiles/R5/Saved/SaveProfile
+  - WindowsGSM\servers\%ID%\serverfiles/R5/Saved/Configs
+- WindowsGSM Config
+  - WindowsGSM\servers\%ID%\configs
+
+### Available Params
+All these params are automatically set by WGSM. Check Serverfiles/Documentation.pdf
+- -port=8211                    can be change by Click Edit Config => Port
+- -queryPort=27015              can be change by Click Edit Config => QueryPort
+- -serverName                   can be change by Click Edit Config => Server Name
+- -nographics 					default
+- -batchmode					default
+
+### Connecting on a locked server (with password)
+For now only Connecting via a the ingame serverbrowser is possible. It is recommended to note down the 5 Options after password and filter your servers accordingly (as there is no name filter either)
+- On the left of the join window be sure to click public (not official)
+- On the lower part choose your options to filter down the list of servers
+- Be sure to favour your server, so they should show up under Favorites on the next time
+
+### Not having an full IPv4 adress ( named CCNAT or DSL Light )
+No game or gameserver supports ipv6 only connections. 
+- You need to either buy one (most VPN services provide that option. A pal uses ovpn.net for his server, I know of nordvpn also providing that. Should both cost around 7€ cheaper half of it, if your already having an VPN)
+- Or you pay a bit more for your internet and take a contract with full ipv4. (depending on your country)
+- There are also tunneling methods, which require acces to a server with a full ipv4. Some small VPS can be obtained, not powerfull enough for the servers themself, but only for forwarding. I think there are some for under 5€), the connection is then done via wireguard. but its a bit configuration heavy to setup) 
+
+Or you connect your friends via VPN to your net and play via local lan then.
+Many windowsgsm plugin creators recommend zerotier (should be a free VPN designated for gaming) , see chapter below (or tailscale, but no howto there)
+
+## How can you play with your friends without port forwarding?
+- Use [zerotier](https://www.zerotier.com/) folow the basic guide and create network
+- Download the client app and join to your network
+- Create static IP address for your host machine
+- Edit WGSM IP Address to your recently created static IP address
+- Give your network ID to your friends
+- After they've joined to your network
+- They can connect using the IP you've created eg: 10.123.17.1:7777
+- Enjoy
+
+### Support
+[WGSM](https://discord.com/channels/590590698907107340/645730252672335893)
+
+### Give Love!
+[Buy me a coffee on Kofi](https://ko-fi.com/raziel7893)
+
+[Paypal](https://paypal.me/raziel7893)
+
+### License
+This project is licensed under the MIT License - see the <a href="https://github.com/raziel7893/WindowsGSM.JediAcademy/blob/main/LICENSE">LICENSE.md</a> file for details
+
+### Thanks
+Thanks to ohmcodes for the Enshrouded and Palworld Plugins which i used for guidance to create this one
